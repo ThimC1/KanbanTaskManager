131ab56fce20e89d4cd03f400c313894fb42001c
ThimHa <thimethbolabotuwa25@gmail.com>
2025-01-22 10:57:46 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import TaskCard from './assets/taskcard';
+
+
 
 function App() {
+
+  const title='Do Market Research'
+  const id="BUS-1"
+  const points=5
+
   return (
-    <div>App</div>
+    <div>
+      <TaskCard title={title} id={id} points={points}/>
+    </div>
   )
 }
 
-export default App
+export default App;
